676cffd0de4fec4c67d0ad059ba9b441ad77f79a
shawna <29107366+shawnanx@users.noreply.github.com>
2025-03-30 16:08:35 -0700

    Add: age_group_description in stg_birth_weight_m_age view

--- dbt/models/staging/stg_birth_weight_m_age.sql
+++ dbt/models/staging/stg_birth_weight_m_age.sql
@@ -6,6 +6,18 @@
 
 SELECT
   CAST(mager9 AS INT64) AS age_group,
+  CASE mager9
+    WHEN 1 THEN 'Under 15'
+    WHEN 2 THEN '15-19'
+    WHEN 3 THEN '20-24'
+    WHEN 4 THEN '25-29'
+    WHEN 5 THEN '30-34'
+    WHEN 6 THEN '35-39'
+    WHEN 7 THEN '40-44'
+    WHEN 8 THEN '45-49'
+    WHEN 9 THEN '50+'
+    ELSE 'Unknown'
+  END AS age_group_description,
   ROUND(AVG(CAST(dbwt AS NUMERIC)), 2) AS avg_birth_weight,
   COUNT(*) AS record_count,
   ROUND(STDDEV(CAST(dbwt AS NUMERIC)), 2) AS std_dev_birth_weight,
